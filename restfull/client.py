a1af94a0d08104c4c7ecb703dd7809dd69cf05c4
Oleg Dolya <oleg.dolya@gmail.com>
2010-05-16 20:22:18 +0300

    Added path_cache modified:   restfull/client.py

--- restfull/client.py
+++ restfull/client.py
@@ -38,59 +38,59 @@ class ConnectionError(Exception):
         return "Connection failed"
 
 class Connection:
-    def __init__(self, base_url, username=None, password=None):
+    def __init__(self, base_url, username=None, password=None, path_cache=".cache"):
         self.base_url = base_url
         self.username = username
         m = mimeTypes()
         self.mimetypes = m.getDictionary()
-        
+
         self.url = urlparse.urlparse(base_url)
-        
+
         (scheme, netloc, path, query, fragment) = urlparse.urlsplit(base_url)
-            
+
         self.scheme = scheme
         self.host = netloc
         self.path = path
-        
+
         # Create Http class with support for Digest HTTP Authentication, if necessary
-        self.h = httplib2.Http(".cache")
+        self.h = httplib2.Http(path_cache)
         self.h.follow_all_redirects = True
         if username and password:
             self.h.add_credentials(username, password)
-    
+
     def request_get(self, resource, args = None, headers={}):
         return self.request(resource, "get", args, headers=headers)
-        
+
     def request_delete(self, resource, args = None, headers={}):
         return self.request(resource, "delete", args, headers=headers)
-        
+
     def request_head(self, resource, args = None, headers={}):
         return self.request(resource, "head", args, headers=headers)
-        
+
     def request_post(self, resource, args = None, body = None, filename=None, headers={}):
         return self.request(resource, "post", args , body = body, filename=filename, headers=headers)
-        
+
     def request_put(self, resource, args = None, body = None, filename=None, headers={}):
         return self.request(resource, "put", args , body = body, filename=filename, headers=headers)
-        
+
     def get_content_type(self, filename):
         extension = filename.split('.')[-1]
         guessed_mimetype = self.mimetypes.get(extension, mimetypes.guess_type(filename)[0])
         return guessed_mimetype or 'application/octet-stream'
-        
+
     def request(self, resource, method = "get", args = None, body = None, filename=None, headers={}):
         params = None
         path = resource
         headers['User-Agent'] = 'Basic Agent'
-        
+
         BOUNDARY = u'00hoYUXOnLD5RQ8SKGYVgLLt64jejnMwtO7q8XE1'
         CRLF = u'\r\n'
-        
+
         if filename and body:
             #fn = open(filename ,'r')
             #chunks = fn.read()
             #fn.close()
-            
+
             # Attempt to find the Mimetype
             content_type = self.get_content_type(filename)
             headers['Content-Type']='multipart/form-data; boundary='+BOUNDARY
@@ -104,19 +104,19 @@ class Connection:
             encode_string.write(body)
             encode_string.write(CRLF)
             encode_string.write(u'--' + BOUNDARY + u'--' + CRLF)
-            
+
             body = encode_string.getvalue()
             headers['Content-Length'] = str(len(body))
         elif body:
             if not headers.get('Content-Type', None):
                 headers['Content-Type']='text/xml'
-            headers['Content-Length'] = str(len(body))        
+            headers['Content-Length'] = str(len(body))
         else:
             if headers.has_key('Content-Length'):
                 del headers['Content-Length']
-            
+
             headers['Content-Type']='text/plain'
-            
+
             if args:
                 if method == "get":
                     path += u"?" + urllib.urlencode(args)
@@ -124,7 +124,7 @@ class Connection:
                     headers['Content-Type']='application/x-www-form-urlencoded'
                     body = urllib.urlencode(args)
 
-            
+
         request_path = []
         # Normalise the / in the url path
         if self.path != "/":
@@ -136,7 +136,7 @@ class Connection:
                 request_path.append(path[1:])
             else:
                 request_path.append(path)
-        
+
         resp, content = self.h.request(u"%s://%s%s" % (self.scheme, self.host, u'/'.join(request_path)), method.upper(), body=body, headers=headers )
         # TODO trust the return encoding type in the decode?
         return {u'headers':resp, u'body':content.decode('UTF-8')}
